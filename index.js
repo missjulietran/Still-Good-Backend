95d1760226c5b615aba49420024c0dd36585caba
Lukas Wan <lukas.gk.wan@gmail.com>
2021-05-03 12:29:40 +0800

    categories and products

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 // const knexConfig = require("./knexfile");
 // const knex = require("knex")(knexConfig.development);
+const fs = require('fs')
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,15 @@ const dashboardRouter = require("./routers/DashboardRouter")(express);
 // Routers
 app.use("/", dataRouter);
 app.use("/", dashboardRouter);
+
+//temporary route
+app.get('/Categories',(req,res)=>{
+  fs.readFile(__dirname+'/data/sections.json',{encoding:'utf-8'},(err,data)=>{
+    let cats=JSON.parse(data)
+    res.send(cats.categories)
+  })
+})
+
 
 app.listen(8080, () => {
   console.log("running 8080");
